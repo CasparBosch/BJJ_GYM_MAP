7d6ec0cc22c53d8dd2321ff5f8b729c034d407fa
CasparBosch <1004288@hr.nl>
2023-06-28 18:23:13 +0200

    tot css issue

--- GymMap/App.js
+++ GymMap/App.js
@@ -1,33 +1,41 @@
-import * as React from "react";
+// import * as React from "react";
+import React, {StrictMode} from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import MapView from "react-native-maps";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/core";
+import { render } from "react-dom";
+
+import "./src/index.css";
 import Map from "./views/map.js";
 import Settings from "./views/settings.js";
 import Collection from "./views/collection.js";
 import Home from "./views/home.js";
 import SideBar from "./src/components/SideBar/index.jsx";
-
+import Channelbar from "./components/ChannelBar/index.jsx";
+import ContentContainer from "./components/ContentContainer/index.jsx";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return (
+    render (
+        <StrictMode>
         <div className="flex">
+            <SideBar />
+            <Channelbar />
+            <ContentContainer />
 
-        <SideBar />
-        
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={Home} />
-                <Stack.Screen name="Map" component={Map} />
-                <Stack.Screen name="Settings" component={Settings} />
-                <Stack.Screen name="Collection" component={Collection} />
-            </Stack.Navigator>
-        </NavigationContainer>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={Home} />
+                    <Stack.Screen name="Map" component={Map} />
+                    <Stack.Screen name="Settings" component={Settings} />
+                    <Stack.Screen name="Collection" component={Collection} />
+                </Stack.Navigator>
+            </NavigationContainer>
         </div>
+        </StrictMode>,
     );
 }
