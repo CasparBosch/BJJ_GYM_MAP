c67e17a1feed413b18b3ec1bb4e0417423f12c21
CasparBosch <1004288@hr.nl>
2023-06-28 16:56:25 +0200

    components

--- GymMap/App.js
+++ GymMap/App.js
@@ -11,6 +11,7 @@ import Collection from "./views/collection.js";
 import Home from "./views/home.js";
 import SideBar from "./src/components/SideBar/index.jsx";
 
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
